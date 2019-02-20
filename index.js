5753c7adc143b09e71db32e765ae01a66b8638e2
benign-product-4738 <alexandra.f.hare@gmail.com>
2019-02-20 20:55:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // add solution here
+function theBeatlesPlay(musicians,instruments){
+  var arr = [];
+  
+  for(var i=0;i<musicians.length;i++){
+    arr.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  
+  return arr;
+}
+
+function johnLennonFacts(facts){
+  var arr= [];
+  
+  var i=0;
+  while(i<facts.length){
+    var sentence = facts[i].concat("!!!");
+    arr.push(sentence);
+    i++;
+  }
+  return arr;
+}
+
+function iLoveTheBeatles(number){
+  var arr = [];
+  do{
+    arr.push("I love the Beatles!");
+    number++;
+  }while(number<15);
+    
+  return arr;
+}
